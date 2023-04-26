3828111d6a890c1208407da3d5031dfec1a2a35b
Jakub Vana <xvanaj@gmail.com>
2023-04-26 10:59:59 +0200

    Removed unused files. Cleanup

--- src/main/java/com/example/recommendationservice/domain/enums/FilterOperation.java
+++
@@ -1,9 +0,0 @@
-package com.example.recommendationservice.domain.enums;
-
-public enum FilterOperation {
-
-    EQUALS,
-    GREATER_OR_EQUAL,
-    LOWER_OR_EQUAL,
-
-}
--- src/main/java/com/example/recommendationservice/domain/enums/FilterType.java
+++
@@ -1,8 +0,0 @@
-package com.example.recommendationservice.domain.enums;
-
-public enum FilterType {
-
-    SYMBOL,
-    DATE,
-
-}
